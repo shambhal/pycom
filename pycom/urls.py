7a65660ae2a591adbd0abbd958c947c5aa2c3b65
shambhal <shambhalnetworks@gmail.com>
2025-05-14 21:14:50 +0530

    Update urls.py

--- pycom/urls.py
+++ pycom/urls.py
@@ -18,9 +18,8 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
-'''from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
-'''
 from .admin import admin_site
 #from customer
 #from api.views import ServiceAPIView
@@ -51,6 +50,9 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path("", include("catalog.urls")),
     path("",include("information.urls")),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
    
 ]
 if settings.DEBUG:
